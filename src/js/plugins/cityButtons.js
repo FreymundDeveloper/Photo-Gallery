c7ef9275549910804d00dbdc5c0f8aeec193c347
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-12-22 19:15:46 -0300

    Gallery Filter Plugin

---
+++ src/js/plugins/cityButtons.js
@@ -0,0 +1,45 @@
+import $ from 'jquery';
+
+const duration = 30;
+
+function filterBtCity(city) {
+    $('[wm-city]').each(function(index, element) {
+        const isTarget = ($(this).attr('wm-city') === city) || (city === null);
+
+        if (isTarget){
+            $(this).parent().removeClass('d-none');
+            $(this).fadeIn(duration);
+        }
+        else {
+            $(this).fadeOut(duration, () => {
+                $(this).parent().removeClass('d-none');
+            });  
+        }      
+    });
+}
+
+$.fn.cityButtons = function() {
+    const cities = new Set;
+
+    $('[wm-city]').each(function(index, element) {
+        cities.add($(element).attr('wm-city'));
+    });
+
+    const buttons = Array.from(cities).map(city => {
+        const button = $('<button>').addClass(['btn', 'btn-info']).html(city);
+        button.click(event => filterBtCity(city));
+        return button;
+    });
+
+    const buttonAll = $('<button>').addClass(['btn', 'btn-info', 'active']).html('All');
+    buttonAll.click(event => filterBtCity(null));
+    buttons.push(buttonAll)
+
+    const buttonGroup = $('<div>').addClass(['btn-group']);
+    buttonGroup.append(buttons)
+
+    $(this).html(buttonGroup);
+    return this;
+}
+
+$('[wm-city-buttons]').cityButtons();
