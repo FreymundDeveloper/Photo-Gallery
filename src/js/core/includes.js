c0b81e16057259d694fbf11e635f889703e22897
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-12-22 19:28:10 -0300

    Plugin Callback

--- src/js/core/includes.js
+++ src/js/core/includes.js
@@ -1,5 +1,11 @@
 import $ from 'jquery';
 
+const loadHtmlSucessCallbacks = [];
+
+export function onLoadHtmlSuccess(callback) {
+    if(!loadHtmlSucessCallbacks.includes(callback)) loadHtmlSucessCallbacks.push(callback);
+}
+
 function loadIncludes(parent = 'body') {
     $(parent).find('[wm-include]').each(function(index, element) {
         const url = $(element).attr('wm-include');
@@ -9,6 +15,7 @@ function loadIncludes(parent = 'body') {
                 $(element).html(data);
                 $(element).removeAttr('wm-include');
 
+                loadHtmlSucessCallbacks.forEach(callback => callback(data));
                 loadIncludes(element);
             }
         });
